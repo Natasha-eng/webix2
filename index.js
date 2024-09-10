a2741cac29333b6632de11ec247044fa888c96a3
Natasha Shilova <natasha.shilova.89@gmail.com>
2024-09-10 18:04:30 +0300

    create custom button

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+const customButtom = webix.protoUI({
+    name: "threestateButton",
+
+    defaults: {
+        value: "Off",
+    },
+
+    $init: function (config) {
+
+        this.$ready.push(this.initState);
+        this.attachEvent("onStateChange", function (id) {
+            this.select(id);
+        });
+        console.log("config", config)
+    },
+
+    initState: function () {
+     
+    },
+
+    onStateChange: function (state) {
+
+    }
+
+
+
+}, webix.ui.button);
+
 webix.ready(function () {
     webix.ui({
         id: "app",
-        
+        rows: [{
+            view: "threestateButton",
+        }]
     });
 });
